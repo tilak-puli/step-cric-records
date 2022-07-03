916e6bff8e48fb023539fbb9c2447bfc800754a9
Tilak Puli <tilakpuli15@gmail.com>
2022-07-03 11:58:40 +0530

    Add july 2 matches

--- ui/data/allMatches.ts
+++ ui/data/allMatches.ts
@@ -1,3 +1,4 @@
 import matches0  from "./matches0.json";
 import matches1  from "./matches1.json";
-export default [...matches0, ...matches1]
+import matches2  from "./matches2.json";
+export default [...matches0, ...matches1, ...matches2]
