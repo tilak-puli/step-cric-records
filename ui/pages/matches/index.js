678b7df0008f88e9af5040dc17fbb97017d6f9b5
Tilak Puli <tilakpuli15@gmail.com>
2022-07-14 12:12:17 +0530

    Add matches page and tags

--- ui/pages/matches/index.js
+++ ui/pages/matches/index.js
@@ -10,8 +10,8 @@ const Matches = () => {
     <Wrap spacing={10} m={[5, 10]}>
       {matches
         .map((m, i) => (
-          <Box w={["100%", 400]}>
-            <MatchCard id={i} match={m} key={i} />
+          <Box w={["100%", 400]} key={i}>
+            <MatchCard id={i} match={m} />
           </Box>
         ))
         .reverse()}
