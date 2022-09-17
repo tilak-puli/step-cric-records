5540794863a23be2bdf4ae28bfe43ecca4022d52
Sourav Banerjee <99480048+souravwithstars@users.noreply.github.com>
2022-09-17 10:34:20 +0530

    Added 16-Sep-2022 matches

--- ui/data/allMatches.ts
+++ ui/data/allMatches.ts
@@ -1,4 +1,5 @@
-import matches0 from "./matches0.json";
-import matches1 from "./matches1.json";
-import matches2 from "./matches2.json";
-export default [...matches0, ...matches1, ...matches2]
+import matches0  from "./matches0.json";
+import matches1  from "./matches1.json";
+import matches2  from "./matches2.json";
+import matches3  from "./matches3.json";
+export default [...matches0, ...matches1, ...matches2, ...matches3]
