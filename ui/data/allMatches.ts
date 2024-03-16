ef768aabb7a39446da7adfa15af66af63f154e6b
Tilak Puli <tilakpuli15@gmail.com>
2024-03-16 11:53:59 +0530

    mar 10

--- ui/data/allMatches.ts
+++ ui/data/allMatches.ts
@@ -4,4 +4,5 @@ import matches2  from "./matches2.json";
 import matches3  from "./matches3.json";
 import matches4  from "./matches4.json";
 import matches5  from "./matches5.json";
-export default [...matches0, ...matches1, ...matches2, ...matches3, ...matches4, ...matches5]
+import matches6  from "./matches6.json";
+export default [...matches0, ...matches1, ...matches2, ...matches3, ...matches4, ...matches5, ...matches6]
